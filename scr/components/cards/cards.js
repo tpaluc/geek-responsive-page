6af70aa8b365243e7f5e5be38a050adbef165a2c
Tomasz <tomgaek@gmail.com>
2023-11-29 22:00:35 +0000

    add eslint rules

--- scr/components/cards/cards.js
+++ scr/components/cards/cards.js
@@ -1,24 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const links = Array.from(document.querySelectorAll('.navBar__listElementLink'));
-    const grids = Array.from(document.querySelectorAll('.container'));
-  
-    links.forEach((element, index) => {
-      element.onclick = () => {
-        links.forEach(el => {
-          el.classList.remove('navBar__listElementLink-clicked');
-        });
-        element.classList.add('navBar__listElementLink-clicked');
-        grids.forEach(elem => {
-          elem.classList.remove('container-active', 'container-hidden');
-        });
-        grids[index].classList.add('container-hidden');
-        setTimeout(()=>{
-          grids[index].classList.remove('container-hidden')
-          grids[index].classList.add('container-active');
-        }, 100);
-      };
-    });
-  });
+  const links = Array.from(document.querySelectorAll('.navBar__listElementLink'));
+  const grids = Array.from(document.querySelectorAll('.container'));
 
-  
+  links.forEach((element, index) => {
+    element.onclick = () => {
+      links.forEach(el => {
+        el.classList.remove('navBar__listElementLink-clicked');
+      });
+      element.classList.add('navBar__listElementLink-clicked');
+      grids.forEach(elem => {
+        elem.classList.remove('container-active', 'container-hidden');
+      });
+      grids[index].classList.add('container-hidden');
+      setTimeout(() => {
+        grids[index].classList.remove('container-hidden');
+        grids[index].classList.add('container-active');
+      }, 100);
+    };
+  });
+});
