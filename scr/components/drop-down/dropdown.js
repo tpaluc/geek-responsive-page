75ee129c0eddb168e71e33d7336324a96abb3697
Tomasz <tomgaek@gmail.com>
2023-11-29 21:42:14 +0000

    update js code: use eventlisteners and change level of forEach loop(use it on main element)

--- scr/components/drop-down/dropdown.js
+++ scr/components/drop-down/dropdown.js
@@ -1,56 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const dropdown = Array.from(document.querySelectorAll('.dropdownContainer__dropdown'));
 
-    const link = Array.from(document.querySelectorAll('.dropdownContainer__button'));
-    const dropdown = Array.from(document.querySelectorAll('.dropdownContainer__dropdown'));
-    const dropdownLink = Array.from(document.querySelectorAll('.dropdownContainer__listElement'));
-  
-    const class1 = 'dropdownContainer__dropdown-show';
-  
-    let mouseOver = false;
-  
-    // add listener to each link
-    const addListener = (array, array2, cssClass) => {
-      array.forEach((element, index) => {
-        element.onmouseover = () => active(index, array2, cssClass);
-      });
+  const class1 = 'dropdownContainer__dropdown-show';
+  const class2 = 'dropdownContainer__dropdown-hide';
+
+  document.querySelectorAll('.dropdownContainer').forEach((container, index) =>{
+
+    // add listener to each button
+    const addListener = (array, cssClass1, cssClass2) => {
+        container.addEventListener( 'pointerover', () => active(index, array, cssClass1, cssClass2));
     };
-  
+
     // display list assigned to the link that mouse is hovering over
-    const active = (index, item, cssClass) => {
-      mouseOver = true;
-      const dontShow = [...item.slice(0, index), ...item.slice(index + 1)];
+    const active = (index, array, cssClass1, cssClass2) => {
+      const dontShow = [...array.slice(0, index), ...array.slice(index + 1)];
       dontShow.forEach(element => {
-        element.classList.remove(cssClass);
+        element.classList.remove(cssClass1, cssClass2);
       });
-      item[index].classList.toggle(cssClass);
+      array[index].classList.add(cssClass2);
+      setTimeout(()=>{
+        array[index].classList.add(cssClass1);
+      },10)   
     };
-  
-    // check if mouse is still hovering over element
-    const hover = (array, cssClass) => {
-      if (!mouseOver) {
-        array.forEach(element => {
-          element.classList.remove(cssClass);
-        });
-      }
-      mouseOver = false;
-    };
-  
-    // check if mouse is hovering over element's list
-    const mouseIsHovering = array => {
-      array.forEach(element => {
-        element.onmouseover = () => {
-          mouseOver = true;
-        };
-      });
-    };
-  
-    // Settings for all dropdown lists:
-    document.onmouseover = () => {
-      hover(dropdown, class1);
-    };
-  
-    addListener(link, dropdown, class1);
-  
-    mouseIsHovering(dropdownLink);
-  });
+
+    // check if mouse is still hovering over the container, if not: hide it
+    const hovering = (element, array, cssClass1, cssClass2) =>{
+        element.addEventListener("pointerout", ()=>{
+          array.forEach(element => {
+            element.classList.remove(cssClass1, cssClass2);
+          });
+        })
+    }
+
+    addListener(dropdown, class1, class2);
+
+    hovering(container, dropdown, class1, class2)
+  })
+})
   
